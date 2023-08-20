a8125e372536afcae03dbb311df814fa1195df02
Shreejesh <tshreejesh@gmail.com>
2023-08-20 22:10:39 +0530

    first commit

--- javascript/even.js
+++ javascript/even.js
@@ -1,9 +1,18 @@
-const arr=[1,2,3,4,5,6,7,8,9,10];
-var count=0;
-for(let i=0;i<arr.length;i++)
-{
-    if((arr[i]%2)==0){
-        count++;
+function countEvenNumbers(arr) {
+
+    // Your code here
+    var count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] % 2) == 0) {
+            count++;
+        }
     }
+    console.log("Count of Even Number is " + count);
 }
-console.log("Count of Even Number is "+count);
+// Example usage
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6]));
+// Output: 3
+console.log(countEvenNumbers([10, 21, 32, 45]));
+// Output: 2
+console.log(countEvenNumbers([7, 9, 11, 13]));
+    // Output: 0
--- javascript/maximum.js
+++ javascript/maximum.js
@@ -1,9 +1,23 @@
-const arr=[10,20,25,30,90,80,84];
-var max=0;
-for(let i=0;i<arr.length;i++){
-    if(max<arr[i]){
-        max=arr[i];
+function findMaxValue(arr) {
+    // Your code here
+    
+    var max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+
     }
     
+    console.log("maximum is " + max);
 }
-console.log("maximum is "+max)
+// Example usage
+console.log(findMaxValue([5, 9, 2, 6, 3]));
+// Output: 9
+
+console.log(findMaxValue([-10, -5, -2, -8]));
+// Output: -2
+
+console.log(findMaxValue([100, 87, 92, 105]));
+// Output: 105
+
